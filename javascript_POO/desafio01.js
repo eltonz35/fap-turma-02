23a886092d58bc7366b9caf6e11f4aaed7ca97dc
Elthon <eltonsilva.mecatronic@gmail.com>
2023-10-13 22:04:35 -0300

    desafio 01

---
+++ javascript_POO/desafio01.js
@@ -0,0 +1,22 @@
+/**
+ * Vamos criar um cadastro de pessoa com os seguintes fados: nome, salário, idade e
+ * se possui diploma. Antes de começar a codificação, identifiquem os tipos de cada
+ * variável. Em seguida, desenvolvam o código utilizando exemplos para cada vaiável que
+ * respeitem os tipos identificados. Certifiquem-se de que os valores correspondam aos
+ * tipos de dados corretos.
+ */
+
+var nome = String;
+var salario = Number;
+var idade = Number;
+var posuiDiploma = Boolean;
+
+nome = "Elthon";
+salario = 1320;
+idade = 28;
+posuiDiploma = false;
+
+console.log(nome);
+console.log(salario);
+console.log(idade);
+console.log(posuiDiploma);
